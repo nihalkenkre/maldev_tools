e41ae7afc573354449a01fee79dfc4ee3ec0bc7a
Nihal Kenkre <nihal.kenkre@gmail.com>
2024-08-19 11:47:37 +0530

    added 32 bit mode hash: hash value for use with 4 byte registers

--- hash/fold_hash.py
+++ hash/fold_hash.py
@@ -1,10 +1,32 @@
 import argparse
 
 
-def main(args):
+def hash_32(input):
     hash = 0
 
-    input_bytes = bytes(args.input, encoding='utf-8')
+    input_bytes = bytes(input, encoding='utf-8')
+    input_bytes_len = len(input_bytes)
+
+    i = 0
+    while i < input_bytes_len:
+        current_fold = input_bytes[i]
+        current_fold <<= 8
+
+        if i + 1 < input_bytes_len:
+            current_fold |= input_bytes[i + 1]
+            current_fold <<= 8
+
+        hash += current_fold
+
+        i += 2
+
+    return hash
+
+
+def hash_64(input):
+    hash = 0
+
+    input_bytes = bytes(input, encoding='utf-8')
     input_bytes_len = len(input_bytes)
 
     i = 0
@@ -27,12 +49,21 @@ def main(args):
 
         i += 4
 
-    print(hex(hash), hash)
+    return hash
+
+
+def main(args):
+
+    hash32 = hash_32(args.input)
+    hash64 = hash_64(args.input)
+
+    print(f'32 bit hash {hex(hash32)}')
+    print(f'64 bit hash {hex(hash64)}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        add_help = 'Returns a folded hash value of the input string', epilog = 'Happy Hashing')
+        add_help='Returns a folded hash value of the input string', epilog='Happy Hashing')
     parser.add_argument('input')
 
     main(parser.parse_args())
